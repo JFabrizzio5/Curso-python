621b1865f49bc2a7465608542af3f450b32bac30
Joseph Fabrizzio <115313375+JFabrizzio5@users.noreply.github.com>
2023-11-22 22:23:21 -0600

    Primer commit con basic1
    Se agrega la introduccion del primer dia de codigo

---
+++ Basic1/variables.py
@@ -0,0 +1,42 @@
+#Objetivo: aprender como se definen las variables y comentarios
+
+#Un comentario es algo que no se va a ejecutar en el codigo final entonces nos sirve para tomar notas y recordar luego nuestro codigo
+
+#Una variable es un espacio en memoria que nos 
+#ayuda a memorizar o lamacenar datos por ejemplo hablar o hacer instrucciones guarda un espacio en el cerebro
+
+#Este es un tipo de variable "Strign" este tipo de variable
+#se ocupa para hacer texto 
+nombre_variable = "Hola Compa"
+
+
+#Esta variable es tipo entero es decir solo numeros enteros
+#Normalmente la ocuparemos para cosas simpes sumas restas o ver si algo es de cierto valor
+
+Numero1 = 1
+
+#Esta variable es tipo flooat por lo cual tiene puntos decimales
+#con este se pueden hacer operaciones exactas 
+
+Puntuaje = 1342.23
+
+
+#Esta es una variable tipo boolean que solo tiene cierto o falso 
+#o si y no se utiliza para saber si algo es verdadero o falso 
+
+Tomar_Droga_Es_Bueno= False
+Tomar_Aguita_Es_Bueno = True
+
+#Este comando sirve para imprimir lo que sea en consola
+
+print("Hola buen dia amigo")
+
+#De esta forma podemos imprimir una variable que ya tenemos hecha
+
+print(nombre_variable)
+
+#Tambien podemos agregar variables de
+
+print("No te salude?",nombre_variable, ", perdon solo se que mi puntuaje fue de",Puntuaje, "en el examen")
+
+#Nota los textos solo van entre "" para decir que estamos diciendo al programa que eso es un texto 
