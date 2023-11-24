51a3fc7617dc038d2afe707c1179f9896f854f7d
Joseph Fabrizzio <115313375+JFabrizzio5@users.noreply.github.com>
2023-11-24 17:43:03 -0600

    Update Strings.py
    Ejemplo codigo incompleto

---
+++ Basic1/Strings.py
@@ -0,0 +1,8 @@
+#Este archivo es para que los strings y explicar como usarlos y saber como formatearlos 
+
+String = "Los arrays son textos o una variable que puede contener un texto"
+print(type(String))
+
+#Con la propiedad type podemos ver que es un tipo string
+
+String
